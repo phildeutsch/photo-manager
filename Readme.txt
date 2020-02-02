a0e4f6eeed12749d75daadbe5a7b2dcbbf3c5449
Phil <phil.deutsch@protonmail.com>
2020-02-02 13:18:57 +0000

    use date instead of str

---
+++ Readme.txt
@@ -0,0 +1,16 @@
+1.	Add exif
+	> python add_exif.py
+	
+2. Move photos
+	PhotoMove
+	Step 1: D:\Dropbox\Photos Backup
+	Step 2:	D:\Dropbox\Photos
+	Step 3: Find Files
+	Step 4: Output folder structure: Yr, Mo
+	Step 5: Duplicates: overwrite
+	Step 6: Do not move files without Exif
+	
+3. Catalogue giles in ACDSee
+
+4. Sync to NUC
+	
--- add_exif.py
+++ add_exif.py
@@ -3,39 +3,36 @@ import piexif
 
 import os
 import time
-
-folder = '../P Whatsapp'
-
-def get_datetime(filename):
-    date_str = filename.split('-')[1]
-    return datetime.strptime(date_str, '%Y%m%d')
+import re
 
 def get_date(filename):
-    date_str = filename.split('-')[1]
-    return datetime.strptime(date_str, '%Y%m%d').strftime("%Y:%m:%d %H:%M:%S")
+	date_str = re.search(r'\d+', filename).group()
+	return datetime.strptime(date_str, '%Y%m%d').strftime("%Y:%m:%d %H:%M:%S")
 
-
-allowedFileEndings = ['mp4','jpg','3gp','jpeg']
-
-filenames = [fn for fn in os.listdir(folder) if fn.split('.')[-1] in allowedFileEndings]
-filenames = [fn for fn in filenames if 'WA' in fn]
+folder = 'D://Dropbox//Photos Backup/'
+filenames = [os.path.join(root, name)
+             for root, dirs, files in os.walk(folder)
+             for name in files
+             if name.endswith((".mp4", ".jpg", ".jpeg"))]
 
 l = len(filenames)
 print(l)
 
-for i, filename in enumerate(filenames):
+for filename in filenames:
+	print(filename, end = '\t')
 	try:
-		if filename.endswith('mp4') or filename.endswith('3gp'):
-			date = get_datetime(filename)
-			modTime = time.mktime(date.timetuple())
-			os.utime(folder + '/' + filename, (modTime, modTime))
+		if filename.endswith('mp4'):
+			date = get_date(filename)
+			modTime = time.mktime(datetime.strptime(date, "%Y:%m:%d %H:%M:%S").timetuple())
+			os.utime(filename, (modTime, modTime))
 
 		elif filename.endswith('jpg') or filename.endswith('jpeg'):
 			exif_dict = {'Exif': {piexif.ExifIFD.DateTimeOriginal: get_date(filename)}}
 			exif_bytes = piexif.dump(exif_dict)
-			piexif.insert(exif_bytes, folder + '/' + filename)
+			piexif.insert(exif_bytes, filename)
 
-		print('{}: {}/{}'.format(filename, i + 1, l))
+		print('OK')
 	except:
+		print('ERROR')
 		pass
 print('\nDone!')
